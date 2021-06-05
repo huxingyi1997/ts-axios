9a460f4070943ac885ba69d19062173a975f7d23
huxingyi1997 <huxingyi1997@sina.com>
2021-06-05 12:03:01 +0800

    [build] 0.1.2

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,6 +4,7 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import { terser } from 'rollup-plugin-terser'
 
 const pkg = require('./package.json')
 
@@ -26,14 +27,22 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({
+      typescript: require('typescript'),
+      objectHashIgnoreUnknownHack: true,
+      useTsconfigDeclarationDir: true
+    }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
+    terser({
+      compress: {
+        pure_funcs: ['console.log'] // 去掉console.log函数
+      }
+    }),
     // Resolve source maps to the original source
     sourceMaps()
   ]
